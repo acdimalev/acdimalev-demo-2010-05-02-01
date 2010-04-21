fcc139df22c5893a2c21fc2c5ebde4b6a446e4f6
Sam Rodgers <acdimalev@gmail.com>
2010-04-21 12:15:48 -0700

    Added controls for moving.

--- foo.c
+++ foo.c
@@ -6,7 +6,7 @@
 int fps = 30;
 int w = 320;
 int h = 240;
-float scale = 8.0;
+float scale = 16.0;
 
 int main(int argc, char **argv) {
   SDL_Surface *sdl_surface;
@@ -17,7 +17,10 @@ int main(int argc, char **argv) {
   Uint8 *keystate;
   Uint32 next_frame, now;
 
-  float theta, t, taccel, tvel, tdrag, x, y, xn, yn;
+  float theta, t, taccel, tvel, tdrag, x, y;
+  float accel, xvel, yvel, drag;
+
+  float vertx, verty, tempx, tempy;
 
   int running;
 
@@ -47,8 +50,11 @@ int main(int argc, char **argv) {
   /* Game Logic */
   running = 1;
   t = M_PI_4;
-  tdrag = 63/64.0;
+  x = 1; y = 1;
   taccel = 4;
+  tdrag = 63/64.0;
+  accel = 1 << 7;
+  drag = 63/64.0;
 
   SDL_LockSurface(sdl_surface);
   while (running) {
@@ -58,29 +64,36 @@ int main(int argc, char **argv) {
     cairo_paint(cr);
     cairo_set_operator(cr, CAIRO_OPERATOR_OVER);
 
-    theta = 2*M_PI * (0/3.0); x = sin(theta); y = cos(theta);
-    xn = x*cos(t) - y*sin(t); yn = x*sin(t) + y*cos(t);
-    cairo_move_to(cr, xn, yn);
-    theta = 2*M_PI * (1/3.0); x = sin(theta); y = cos(theta);
-    xn = x*cos(t) - y*sin(t); yn = x*sin(t) + y*cos(t);
-    cairo_line_to(cr, xn, yn);
-    theta = 2*M_PI * (2/3.0); x = sin(theta); y = cos(theta);
-    xn = x*cos(t) - y*sin(t); yn = x*sin(t) + y*cos(t);
-    cairo_line_to(cr, xn, yn);
+    theta = 2*M_PI * (0/3.0); tempx = sin(theta); tempy = cos(theta);
+    vertx = x + tempx*cos(t) - tempy*sin(t);
+    verty = y + tempx*sin(t) + tempy*cos(t);
+    cairo_move_to(cr, vertx, verty);
+    theta = 2*M_PI * (1/3.0); tempx = sin(theta); tempy = cos(theta);
+    vertx = x + tempx*cos(t) - tempy*sin(t);
+    verty = y + tempx*sin(t) + tempy*cos(t);
+    cairo_line_to(cr, vertx, verty);
+    theta = 2*M_PI * (2/3.0); tempx = sin(theta); tempy = cos(theta);
+    vertx = x + tempx*cos(t) - tempy*sin(t);
+    verty = y + tempx*sin(t) + tempy*cos(t);
+    cairo_line_to(cr, vertx, verty);
     cairo_close_path(cr);
 
-    theta = 2*M_PI * (1/3.0); x =  0.1; y = cos(theta) / 2.0;
-    xn = x*cos(t) - y*sin(t); yn = x*sin(t) + y*cos(t);
-    cairo_move_to(cr, xn, yn);
-    theta = 2*M_PI * (1/3.0); x = -0.1; y = cos(theta) / 2.0;
-    xn = x*cos(t) - y*sin(t); yn = x*sin(t) + y*cos(t);
-    cairo_line_to(cr, xn, yn);
-    theta = 2*M_PI * (1/3.0); x = -0.1; y = cos(theta);
-    xn = x*cos(t) - y*sin(t); yn = x*sin(t) + y*cos(t);
-    cairo_line_to(cr, xn, yn);
-    theta = 2*M_PI * (1/3.0); x =  0.1; y = cos(theta);
-    xn = x*cos(t) - y*sin(t); yn = x*sin(t) + y*cos(t);
-    cairo_line_to(cr, xn, yn);
+    theta = 2*M_PI * (1/3.0); tempx =  0.1; tempy = cos(theta) / 2.0;
+    vertx = x + tempx*cos(t) - tempy*sin(t);
+    verty = y + tempx*sin(t) + tempy*cos(t);
+    cairo_move_to(cr, vertx, verty);
+    theta = 2*M_PI * (1/3.0); tempx = -0.1; tempy = cos(theta) / 2.0;
+    vertx = x + tempx*cos(t) - tempy*sin(t);
+    verty = y + tempx*sin(t) + tempy*cos(t);
+    cairo_line_to(cr, vertx, verty);
+    theta = 2*M_PI * (1/3.0); tempx = -0.1; tempy = cos(theta);
+    vertx = x + tempx*cos(t) - tempy*sin(t);
+    verty = y + tempx*sin(t) + tempy*cos(t);
+    cairo_line_to(cr, vertx, verty);
+    theta = 2*M_PI * (1/3.0); tempx =  0.1; tempy = cos(theta);
+    vertx = x + tempx*cos(t) - tempy*sin(t);
+    verty = y + tempx*sin(t) + tempy*cos(t);
+    cairo_line_to(cr, vertx, verty);
     cairo_close_path(cr);
 
     cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
@@ -110,9 +123,17 @@ int main(int argc, char **argv) {
     if (keystate[SDLK_RIGHT]) {
       tvel = tvel - taccel*2*M_PI / fps;
     }
+    if (keystate[SDLK_UP]) {
+      xvel = accel * -sin(t) / fps;
+      yvel = accel * cos(t) / fps;
+    }
 
     t = t + tvel / fps;
+    x = x + xvel / fps;
+    y = y + yvel / fps;
     tvel = tvel * pow(1.0 - tdrag, 1.0/fps);
+    xvel = xvel * pow(1.0 - drag, 1.0/fps);
+    yvel = yvel * pow(1.0 - drag, 1.0/fps);
 
   }
   SDL_UnlockSurface(sdl_surface);
